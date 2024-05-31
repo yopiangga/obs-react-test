5f886dfe08a17ff654591de238305fa8a4a57025
yopiangga <yopigambyok@gmail.com>
2024-05-31 11:15:36 +0700

    Design: Footer

--- src/widgets/layout/footer.tsx
+++ src/widgets/layout/footer.tsx
@@ -1,5 +1,4 @@
 import { Typography } from "@material-tailwind/react";
-import { HeartIcon } from "@heroicons/react/24/solid";
 
 export function Footer({}) {
   const year = new Date().getFullYear();
@@ -8,9 +7,7 @@ export function Footer({}) {
     <footer className="py-2">
       <div className="flex w-full flex-wrap items-center justify-center gap-6 px-2 md:justify-between">
         <Typography variant="small" className="font-normal text-inherit">
-          &copy; {year}, made with{" "}
-          <HeartIcon className="-mt-0.5 inline-block h-3.5 w-3.5 text-red-600" />{" "}
-          by Alfian Prisma Yopiangga
+          &copy; {year}, Dashboard by Alfian Prisma Yopiangga
         </Typography>
       </div>
     </footer>
