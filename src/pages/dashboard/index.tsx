6c2a972090cdba207b01054ab996c3db7f895bf3
yopiangga <yopigambyok@gmail.com>
2024-05-31 10:14:20 +0700

    Setup: pages

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,4 @@
+export { HomePage } from "src/pages/dashboard/home";
+export { UsersPage } from "src/pages/dashboard/users";
+export { ProfilePage } from "src/pages/dashboard/profile";
+export { UserEditPage } from "src/pages/dashboard/users/user-edit";
